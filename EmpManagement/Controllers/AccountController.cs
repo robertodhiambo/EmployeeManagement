ec6c798bb61dfb6bf97c0f7b55759829d877cdf3
Robert <robertodhiambo661@gmail.com>
2023-03-29 16:27:46 +0300

    Adding Identity UserManager and SignInManager

--- EmpManagement/Controllers/AccountController.cs
+++ EmpManagement/Controllers/AccountController.cs
@@ -1,14 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmpManagement.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmpManagement.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, 
+                                                        SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager=userManager;
+            this.signInManager=signInManager;
+        }
 
         [HttpGet]
         public IActionResult Register ( )
         {
             return View ( );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register (RegisterViewModel viewModel )
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser 
+                {
+                    UserName = viewModel.Email,
+                    Email = viewModel.Email
+                };
+
+                var result = await userManager.CreateAsync (user, viewModel.Password );
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View ( viewModel );
         }
     }
 }
