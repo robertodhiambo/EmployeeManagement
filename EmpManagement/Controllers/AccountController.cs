8d612c0bc62580e7419ed35e5161966c9088e190
Robert <robertodhiambo661@gmail.com>
2023-03-29 15:37:43 +0300

    register new user page

---
+++ EmpManagement/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Register ( )
+        {
+            return View ( );
+        }
+    }
+}
